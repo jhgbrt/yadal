709eba95ff3fa59ece9ed1366eb5c2edc1b77705
Jeroen.haegebaert <Jeroen.haegebaert@CND5061WPS>
2016-09-08 17:29:32 +0200

    fixed integration tests (keep connection alive when using sqlite + use correct object name)

--- Net.Code.ADONet.Tests.Integration/DbTest.cs
+++ Net.Code.ADONet.Tests.Integration/DbTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Net.Code.ADONet.Extensions;
 using Net.Code.ADONet.Extensions.SqlClient;
 
@@ -11,45 +12,36 @@ namespace Net.Code.ADONet.Tests.Integration
     public class DbTest
     {
         private readonly IDatabaseImpl _target;
-
+        private readonly IDb _db;
         public DbTest(IDatabaseImpl target)
         {
             _target = target;
+            _db = CreateDb();
         }
 
         public void CreateTables()
         {
-            using (var db = CreateDb())
-            {
-                db.Sql(_target.CreatePersonTable).AsNonQuery();
-                db.Sql(_target.CreateAddressTable).AsNonQuery();
-            }
+            _db.Sql(_target.CreatePersonTable).AsNonQuery();
+            _db.Sql(_target.CreateAddressTable).AsNonQuery();
         }
 
         public void DropTables()
         {
-            using (var db = CreateDb())
-            {
-                db.Execute($"DROP TABLE {nameof(Person)}");
-                db.Execute($"DROP TABLE {nameof(Address)}");
-            }
+            _db.Execute($"DROP TABLE {nameof(Person)}");
+            _db.Execute($"DROP TABLE {nameof(Address)}");
         }
 
-        public void Insert(IEnumerable<Person> items, IEnumerable<Address> addresses)
+        public void Insert(IEnumerable<Person> people, IEnumerable<Address> addresses)
         {
-            using (var db = CreateDb())
-            {
-                db.Insert(items);
-                db.Insert(addresses);
-            }
+            _db.Insert(people);
+            _db.Insert(addresses);
         }
+
         public void Update(IEnumerable<Person> items)
         {
-            using (var db = CreateDb())
-            {
-                db.Update(items);
-            }
+            _db.Update(items);
         }
+
         static Dictionary<Type, Exception> _exceptions = new Dictionary<Type, Exception>();
         internal void Connect()
         {
@@ -57,10 +49,7 @@ namespace Net.Code.ADONet.Tests.Integration
             {
                 try
                 {
-                    using (var db = CreateDb())
-                    {
-                        db.Connect();
-                    }
+                    _db.Connect();
                 }
                 catch (Exception e)
                 {
@@ -73,82 +62,62 @@ namespace Net.Code.ADONet.Tests.Integration
 
         public async Task InsertAsync(IEnumerable<Person> items)
         {
-            using (var db = CreateDb())
+            foreach (var item in items)
             {
-                foreach (var item in items)
-                {
-                        await db.Sql(_target.InsertPerson).WithParameters(item).AsNonQueryAsync();
-                }
+                await _db.Sql(_target.InsertPerson).WithParameters(item).AsNonQueryAsync();
             }
         }
 
         public List<Person> GetAllPeopleGeneric()
         {
-            using (var db = CreateDb())
-            {
-                return db
-                    .Sql(_target.Query<Person>().SelectAll)
-                    .AsEnumerable<Person>()
-                    .ToList();
-            }
+            return _db
+                .Sql(_target.Query<Person>().SelectAll)
+                .AsEnumerable<Person>()
+                .ToList();
         }
 
         public DataTable GetSchemaTable()
         {
-            using (var db = CreateDb())
+            var dataReader = _db
+                .Sql(_target.Query<Person>().SelectAll)
+                .AsReader();
+            using (dataReader)
             {
-                var dataReader = db
-                    .Sql(_target.Query<Person>().SelectAll)
-                    .AsReader();
-                using (dataReader)
-                {
-                    var dt = dataReader.GetSchemaTable();
-                    return dt;
-                }
+                var dt = dataReader.GetSchemaTable();
+                return dt;
             }
         }
 
         public List<Person> GetAllPeopleAsDynamic()
         {
-            using (var db = CreateDb())
-            {
-                return db
-                    .Sql(_target.Query<Person>().SelectAll)
-                    .AsEnumerable()
-                    .Select(d => (Person)_target.Project(d))
-                    .ToList();
-            }
+            return _db
+                .Sql(_target.Query<Person>().SelectAll)
+                .AsEnumerable()
+                .Select(d => (Person) _target.Project(d))
+                .ToList();
         }
+
         public async Task<List<Person>> GetAllPeopleAsDynamicAsync()
         {
-            using (var db = CreateDb())
-            {
-                var people = await db
-                    .Sql(_target.Query<Person>().SelectAll)
-                    .AsEnumerableAsync();
-                return people
-                    .Select(d => (Person)_target.Project(d))
-                    .ToList();
-            }
+            var people = await _db
+                .Sql(_target.Query<Person>().SelectAll)
+                .AsEnumerableAsync();
+            return people
+                .Select(d => (Person) _target.Project(d))
+                .ToList();
         }
 
         public DataTable AsDataTable()
         {
-            using (var db = CreateDb())
-            {
-                return db
-                    .Sql(_target.Query<Person>().SelectAll)
-                    .AsDataTable();
-            }
+            return _db
+                .Sql(_target.Query<Person>().SelectAll)
+                .AsDataTable();
         }
 
         public MultiResultSet<Person, Address> AsMultiResultSet()
         {
-            using (var db = CreateDb())
-            {
-                var result = _target.SelectPersonAndAddress(db);
-                return result;
-            }
+            var result = _target.SelectPersonAndAddress(_db);
+            return result;
         }
 
         private IDb CreateDb()
@@ -158,57 +127,40 @@ namespace Net.Code.ADONet.Tests.Integration
 
         public Person Get(int id)
         {
-            using (var db = CreateDb())
-            {
-                return db
-                    .Sql(_target.Query<Person>().Select)
-                    .WithParameters(new {Id = id})
-                    .Single<Person>();
-            }
+            return _db
+                .Sql(_target.Query<Person>().Select)
+                .WithParameters(new {Id = id})
+                .Single<Person>();
         }
 
         public Person[] GetPeopleById(IEnumerable<int> ids)
         {
-            using (var db = CreateDb())
-            {
-                return db
-                    .Sql("SELECT * FROM Person JOIN @IDs IdSet ON Person.Id = IdSet.Id")
-                    .WithParameter("@IDs", ids.Select(id => new {Id = id}), "IdSet")
-                    .AsEnumerable<Person>()
-                    .ToArray();
-            }
+            return _db
+                .Sql("SELECT * FROM Person JOIN @IDs IdSet ON Person.Id = IdSet.Id")
+                .WithParameter("@IDs", ids.Select(id => new {Id = id}), "IdSet")
+                .AsEnumerable<Person>()
+                .ToArray();
         }
 
         public List<int> GetSomeIds(int n)
         {
-            using (var db = CreateDb())
-            {
-                return db.Sql($"SELECT TOP {n} Id FROM {nameof(Person)}").Select(d => (int) d.Id).ToList();
-            }
+            return _db.Sql($"SELECT TOP {n} Id FROM {nameof(Person)}").Select(d => (int) d.Id).ToList();
         }
 
         public int GetCountOfPeople()
         {
-            using (var db = CreateDb())
-            {
-                return db.Sql($"SELECT count(*) FROM {nameof(Person)}").AsScalar<int>();
-            }
+            return _db.Sql($"SELECT count(*) FROM {nameof(Person)}").AsScalar<int>();
         }
+
         public async Task<int> GetCountOfPeopleAsync()
         {
-            using (var db = CreateDb())
-            {
-                var result = await db.Sql($"SELECT count(*) FROM {nameof(Person)}").AsScalarAsync<int>();
-                return result;
-            }
+            var result = await _db.Sql($"SELECT count(*) FROM {nameof(Person)}").AsScalarAsync<int>();
+            return result;
         }
 
         public void BulkInsert(Person[] list)
         {
-            using (var db = CreateDb())
-            {
-                _target.BulkInsert(db, list);
-            }
+            _target.BulkInsert(_db, list);
         }
     }
 }
--- Net.Code.ADONet/Extensions/Experimental/DbExtensions.cs
+++ Net.Code.ADONet/Extensions/Experimental/DbExtensions.cs
@@ -1,11 +1,13 @@
-﻿namespace Net.Code.ADONet.Extensions
+﻿using System.Collections.Generic;
+
+namespace Net.Code.ADONet.Extensions
 {
     public static class DbExtensions
     {
         /// <summary>
         /// Please note: this is an experimental feature, API may change or be removed in future versions"
         /// </summary>
-        public static void Insert<T>(this IDb db, params T[] items)
+        public static void Insert<T>(this IDb db, IEnumerable<T> items)
         {
             var query = Query<T>.Create(db.ProviderName).Insert;
             Do(db, items, query);
@@ -14,7 +16,7 @@
         /// <summary>
         /// Please note: this is an experimental feature, API may change or be removed in future versions"
         /// </summary>
-        public static void Update<T>(this IDb db, params T[] items)
+        public static void Update<T>(this IDb db, IEnumerable<T> items)
         {
             var query = Query<T>.Create(db.ProviderName).Update;
             Do(db, items, query);
@@ -23,13 +25,13 @@
         /// <summary>
         /// Please note: this is an experimental feature, API may change or be removed in future versions"
         /// </summary>
-        public static void Delete<T>(this IDb db, params T[] items)
+        public static void Delete<T>(this IDb db, IEnumerable<T> items)
         {
             var query = Query<T>.Create(db.ProviderName).Delete;
             Do(db, items, query);
         }
 
-        private static void Do<T>(IDb db, T[] items, string query)
+        private static void Do<T>(IDb db, IEnumerable<T> items, string query)
         {
             var commandBuilder = db.Sql(query);
             foreach (var item in items)
