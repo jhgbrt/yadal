6b7d510ecc3e171e55b1404d324d65ac67e8e009
Jeroen Haegebaert <jeroen@haegebaert.com>
2024-11-17 19:47:33 +0100

    remove single file

--- Net.Code.ADONet.SingleFile/join.cmd
+++
@@ -1,2 +0,0 @@
-joincs --input=..\Net.Code.ADONet --output=.\netfx\Db.cs --preprocessor-directives=NETFRAMEWORK
-joincs --input=..\Net.Code.ADONet --output=.\netstandard\Db.cs --preprocessor-directives=NETSTANDARD
